d5ffb5db8cbb042ba05379ed45b1b08235addbe4
Even Westvang <even@bengler.no>
2014-04-19 22:39:25 +0200

    Add a note

--- scripts/snapshot.sh
+++ scripts/snapshot.sh
@@ -17,7 +17,7 @@ ogr2ogr -f "GeoJSON"  -s_srs 'EPSG:25832' -t_srs 'EPSG:4326' ../app/assets/data/
 
 echo "Getting the boundaries"
 
-# We have both "Skolekretser_f" and "Skolekretser_l". Not sure yet what that means.
+# We have both "Skolekretser_f" and "Skolekretser_l". Not sure yet what that means. They are the same.
 curl "http://od2.pbe.oslo.kommune.no/cgi-bin/skolekretser?LAYERS=Skolekretser_l&TRANSPARENT=TRUE&SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&STYLES=&SRS=EPSG%3A32632&BBOX=564440.0192024,6608598.0223571,635559.9807976,6691401.9776429&WIDTH=630&HEIGHT=733&typename=Skolekretser_f&outputFormat=GML2" > school_boundaries.gml
 rm ../app/assets/data/school_boundaries.json
 ogr2ogr -f "GeoJSON"  -s_srs 'EPSG:25832' -t_srs 'EPSG:4326' ../app/assets/data/school_boundaries.json school_boundaries.gml
