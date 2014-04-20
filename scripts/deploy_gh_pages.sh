855d329be73b950836ee43fa35307f1ea0f12721
Even Westvang <even@bengler.no>
2014-04-20 17:12:38 +0200

    Build compressed files

--- scripts/deploy_gh_pages.sh
+++ scripts/deploy_gh_pages.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+echo "Removing old files"
+
 rm -rf "../what-school-gh-pages/data"
 rm -rf "../what-school-gh-pages/images"
 rm -rf "../what-school-gh-pages/javascripts"
 rm -rf "../what-school-gh-pages/stylesheets"
+
+echo "Building compressed project"
+brunch b -P
 
 cp -R public/ "../what-school-gh-pages/"
 cd ../what-school-gh-pages
